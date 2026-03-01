51f920850d4f846ef2e09b2d4b4831a0a29815f4
luis <Luis.Gonzalez@southeasttech.edu>
2026-03-01 06:53:20 -0600

    Add chalk message

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log ("initial project setup");
+import chalk from "chalk";
+
+console.log(chalk.bold.green("Chalk is working!"));
+console.log(chalk.blue("Repo: Unit 2-3 Git Homework"));
+console.log(chalk.yellow("Two commits? I got it."));
